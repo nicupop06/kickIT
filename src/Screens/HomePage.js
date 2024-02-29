5aee8cfad5be2e998011c46ad99d46541b61f055
nicupop06 <pop.nicolae2001@gmail.com>
2024-02-29 22:12:46 +0200

    Ask for location and store it

--- src/Screens/HomePage.js
+++ src/Screens/HomePage.js
@@ -3,12 +3,18 @@ import { StyleSheet, Text, View, Button } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { db } from "../Config/firebaseConfig";
+import MapView, { Marker, Callout } from "react-native-maps";
+import * as Location from "expo-location";
 
 const usersRef = collection(db, "users");
 
 export default function HomePage({ navigation }) {
+  //For local storage
   const [email, setEmail] = useState("");
   const [user, setUser] = useState(null);
+
+  //For map
+  const [location, setLocation] = useState(null);
 
   //Get email from localstorage (login placed it there)
   useEffect(() => {
@@ -22,6 +28,23 @@ export default function HomePage({ navigation }) {
       .catch((error) => {
         alert("Error retrieving email from AsyncStorage:", error);
       });
+  }, []);
+
+  //Ask for map permissions in order to show it from where the user is
+  useEffect(() => {
+    // Request location permission and get current location
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        alert("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      console.log(status);
+      console.log(location);
+    })();
   }, []);
 
   //Collect the whole user after email is taken from localstorage
