4f6470416e74ae600253b64a35eee3f0bcdd53c4
nicupop06 <pop.nicolae2001@gmail.com>
2024-05-08 18:18:21 +0300

    Chart for entries

--- src/Screens/GymStatsPage.js
+++ src/Screens/GymStatsPage.js
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ActivityIndicator,
+  Dimensions,
+} from "react-native";
 import config from "../Config/config";
 import axios from "axios";
+import { LineChart } from "react-native-chart-kit";
+import moment from "moment";
 
 export default function GymStatsPage({ route }) {
   const [paymentIntents, setPaymentIntents] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { gymName } = route.params;
 
   useEffect(() => {
@@ -15,17 +24,72 @@ export default function GymStatsPage({ route }) {
           gymName: gymName,
         },
       });
-      setPaymentIntents(response.data);
-      console.log(response.data);
+      if (response.data.paymentIntents) {
+        setPaymentIntents(response.data.paymentIntents);
+        console.log(response.data.paymentIntents);
+      } else {
+        console.log("No payment intents available");
+      }
+      setLoading(false);
     }
 
     fetchPaymentIntents();
   }, []);
 
+  function countEntriesForDay(day) {
+    const no = paymentIntents.filter(
+      (item) => moment(item.created * 1000).format("ddd") === day
+    ).length;
+    return no;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hello, Expo!</Text>
-      <Text>Gym ID: {gymName}</Text>
+      <Text>No Entries / Week days</Text>
+      <LineChart
+        data={{
+          labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+          datasets: [
+            {
+              data: [
+                countEntriesForDay("Mon"),
+                countEntriesForDay("Tue"),
+                countEntriesForDay("Wed"),
+                countEntriesForDay("Thu"),
+                countEntriesForDay("Fri"),
+                countEntriesForDay("Sat"),
+                countEntriesForDay("Sun"),
+              ],
+            },
+          ],
+        }}
+        width={Dimensions.get("window").width} // from react-native
+        height={220}
+        yAxisInterval={1} // optional, defaults to 1
+        chartConfig={{
+          backgroundColor: "#e6ffff", // Turquoise background color
+          backgroundGradientFrom: "#e6ffff", // Turquoise gradient start color
+          backgroundGradientTo: "#99e6e6", // Turquoise gradient end color
+          color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`, // Black line color
+          labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`, // Black label color
+          style: {
+            borderRadius: 16,
+          },
+          propsForDots: {
+            r: "6",
+            strokeWidth: "2",
+            stroke: "blue",
+          },
+          propsForBackgroundLines: {
+            strokeWidth: 0
+          }
+        }}
+        bezier
+        style={{
+          marginVertical: 8,
+          borderRadius: 16,
+        }}
+      />
     </View>
   );
 }
@@ -35,6 +99,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#fff",
+    backgroundColor: "#f5fcff",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
   },
 });
